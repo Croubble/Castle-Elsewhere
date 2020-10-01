d8ece176e26c07503193021db6db57fcf4f5d792
Croubble <dazedpidgeon@gmail.com>
2020-10-01 18:33:43 +1000

    fullsprite function extraction complete

--- ThroughTheLookingGlass/win32_platform.cpp
+++ ThroughTheLookingGlass/win32_platform.cpp
@@ -125,7 +125,7 @@ UiDrawInfo ui_draw_info;
 
 
 GamefullspriteDrawInfo fullspriteDraw;
-
+GamefullspriteDrawInfo uiDraw;
 GLuint dotted_VAO;
 GLuint dotted_positions_buffer;
 GLuint dotted_scale_buffer;
@@ -1942,61 +1942,10 @@ int main(int argc, char *argv[])
 		}
 	#pragma endregion
 	#pragma region fullsprite GPU setup
-		fullspriteDraw.num_sprites_drawn = 0;
-		fullspriteDraw.atlas_cpu = (glm::vec4*) memory_alloc(permanent_memory, MAX_NUM_FULL_SPRITES * sizeof(glm::vec4));
 		fullspriteDraw.atlas_mapper = floor_atlas_mapper;
-		fullspriteDraw.final_cpu = (glm::mat4*) memory_alloc(permanent_memory, MAX_NUM_FULL_SPRITES * sizeof(glm::mat4));;
-		shader_use(fullSpriteShader);
-		//just use floor_atlas_mapper for fullsprite_atlas_mapper
-		{
-			glGenVertexArrays(1, &fullspriteDraw.fullsprite_VAO);
-			std::cout << glGetError() << ":261" << std::endl;
-			glBindVertexArray(fullspriteDraw.fullsprite_VAO);
+		fullsprite_generate(fullSpriteShader, permanent_memory, vertices_VBO, vertices_EBO, floor_atlas_mapper, &fullspriteDraw);
+	//	fullsprite_generate(fullSpriteShader, permanent_memory, vertices_VBO, vertices_EBO, floor_atlas_mapper, &fullspriteDraw);
 
-			glBindBuffer(GL_ARRAY_BUFFER, vertices_VBO);
-			glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, vertices_EBO);
-
-			int position = glGetAttribLocation(fullSpriteShader, "pos");
-			std::cout << glGetError() << " this is our position value" << std::endl;
-			glVertexAttribPointer(position, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
-			glEnableVertexAttribArray(position);
-
-			int texCoord = glGetAttribLocation(fullSpriteShader, "inputTexCoord");
-			std::cout << glGetError() << " this is our texCoord value" << std::endl;
-			glVertexAttribPointer(texCoord, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
-			glEnableVertexAttribArray(texCoord);
-
-
-			glGenBuffers(1, &fullspriteDraw.fullspriteMatrixBuffer);
-			glBindBuffer(GL_ARRAY_BUFFER, fullspriteDraw.fullspriteMatrixBuffer);
-			glBufferData(GL_ARRAY_BUFFER, sizeof(glm::mat4) * MAX_NUM_FULL_SPRITES, NULL, GL_DYNAMIC_DRAW);
-
-			int matrixOffset = 3;	//value hardcoded from fullsprite.vs
-			glVertexAttribPointer(matrixOffset, 4, GL_FLOAT, false, 16 * sizeof(float), (void*)0);
-			std::cout << glGetError() << std::endl; 
-			std::cout << matrixOffset << "This is our matrix offset" << std::endl;
-			glVertexAttribPointer(matrixOffset + 1, 4, GL_FLOAT, false, 16 * sizeof(float), (void*)(4 * sizeof(float)));
-
-			glVertexAttribPointer(matrixOffset + 2, 4, GL_FLOAT, false, 16 * sizeof(float), (void*)(8 * sizeof(float)));
-			glVertexAttribPointer(matrixOffset + 3, 4, GL_FLOAT, false, 16 * sizeof(float), (void*)(12 * sizeof(float)));
-			glEnableVertexAttribArray(matrixOffset);
-			glEnableVertexAttribArray(matrixOffset + 1);
-			glEnableVertexAttribArray(matrixOffset + 2);
-			glEnableVertexAttribArray(matrixOffset + 3);
-			glVertexAttribDivisor(matrixOffset, 1);
-			glVertexAttribDivisor(matrixOffset + 1, 1);
-			glVertexAttribDivisor(matrixOffset + 2, 1);
-			glVertexAttribDivisor(matrixOffset + 3, 1);
-
-			glGenBuffers(1, &fullspriteDraw.fullspriteAtlasBuffer);
-			glBindBuffer(GL_ARRAY_BUFFER, fullspriteDraw.fullspriteAtlasBuffer);
-			glBufferData(GL_ARRAY_BUFFER, sizeof(glm::vec4)* MAX_NUM_FULL_SPRITES, NULL, GL_DYNAMIC_DRAW);
-
-			int atlasOffset = glGetAttribLocation(fullSpriteShader, "atlasCoord");
-			glVertexAttribPointer(atlasOffset, 4, GL_FLOAT, false, 4 * sizeof(float), (void*)(0));
-			glEnableVertexAttribArray(atlasOffset);
-			glVertexAttribDivisor(atlasOffset, 1);
-		}
 	#pragma endregion 
 	#pragma region dotted GPU setup
 		dotted_VAO;
